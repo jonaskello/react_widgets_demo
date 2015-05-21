4eb0753a7dfe27db239b4aaba90f7f3c5e60772e
Jonas Kello <jonas.kello@divid.se>
2015-05-22 00:55:40 +0200

    Initial commit

--- web/main.dart
+++ web/main.dart
@@ -7,8 +7,11 @@ import 'package:react/react_client.dart' as reactClient;
 import 'package:react_widgets/react_widgets.dart' as widgets;
 
 main() {
-  reactClient.setClientConfiguration();
-  render(rootComponent({}), querySelector('#content'));
+
+    reactClient.setClientConfiguration();
+    widgets.init();
+    render(rootComponent({}), querySelector('#content'));
+
 }
 
 var rootComponent = registerComponent(() => new RootComponent());
@@ -23,6 +26,7 @@ class RootComponent extends Component {
       "comboboxValue": colors[0],
       "numberPickerValue": 5,
       "multiselectValue": [colors[0], colors[2]],
+      "selectListValue": [colors[0]],
     };
   }
 
@@ -30,7 +34,7 @@ class RootComponent extends Component {
   div({}, [
     h1({"style": {"color": "red"}}, "DEMO"),
     div({"style": {"padding": "10px"}}, [
-      "dropdownListValue: ${this.state["dropdownListValue"]}",
+      "DropdownList: ${this.state["dropdownListValue"]}",
       this.state["value"],
       widgets.dropdownList({
         "style": {"width": "200px"},
@@ -40,7 +44,7 @@ class RootComponent extends Component {
       }),
     ]),
     div({"style": {"padding": "10px"}}, [
-      "comboboxValue: ${this.state["comboboxValue"]}",
+      "Combobox: ${this.state["comboboxValue"]}",
       widgets.combobox({
         "style": {"width": "200px"},
         "data": colors,
@@ -48,7 +52,7 @@ class RootComponent extends Component {
         "onChange": (value) => this.setState({"comboboxValue": value})}),
     ]),
     div({"style": {"padding": "10px"}}, [
-      "numberPickerValue: ${this.state["numberPickerValue"]}",
+      "NumberPicker: ${this.state["numberPickerValue"]}",
       widgets.numberPicker({
         "style": {"width": "200px"},
         "format": (v, c) => v,
@@ -57,12 +61,29 @@ class RootComponent extends Component {
         "onChange": (value) => this.setState({"numberPickerValue": value})}),
     ]),
     div({"style": {"padding": "10px"}}, [
-      "multiselectValue: ${this.state["multiselectValue"]}",
+      "Multiselect: ${this.state["multiselectValue"]}",
       widgets.multiselect({
         "style": {"width": "200px"},
         "data": colors,
         "value": this.state["multiselectValue"],
         "onChange": (value) => this.setState({"multiselectValue": value})}),
+    ]),
+    div({"style": {"padding": "10px"}}, [
+      "SelectList: ${this.state["selectListValue"]}",
+      widgets.selectList({
+        "style": {"width": "200px"},
+        "data": colors,
+        "value": this.state["selectListValue"],
+        "onChange": (value) => this.setState({"selectListValue": value})}),
+    ]),
+    div({"style": {"padding": "10px"}}, [
+      "Calendar: ${this.state["calendarValue"]}",
+      widgets.calendar({
+        "style": {"width": "200px"},
+//        "headerFormat": (v, c) => v,
+        "parse": (v, c) => v,
+        "value": this.state["calendarValue"],
+        "onChange": (value) => this.setState({"calendarValue": value})}),
     ]),
   ]);
 
